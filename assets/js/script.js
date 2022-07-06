71748a653ff7b7e5279c5e23e0b65b83707cc3c3
nick-dolf <nickdolf@gmail.com>
2022-07-05 19:06:29 -0500

    added basic expanding search bar for tablet plus

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,12 @@ function searchToggle() {
   header.classList.toggle("search-clicked")
   header.classList.remove("menu-clicked")
 }
+
+
+
+const searchForm = document.querySelector(".form-search");
+const searchInput = document.querySelector(".search__input");
+function toggleSearchInput() {
+  searchForm.classList.toggle("active")
+  searchInput.focus()
+}
